20cce56583d73bb0fc26cda6551c312ae97d9387
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-22 16:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const newArray = [];
+  drivers.map(function(x){
+    newArray.push(x.toLowerCase());
+  });
+  return newArray;
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return {firstName: driverFirst, lastName: driverLast};
+  });
+  
+}
+
